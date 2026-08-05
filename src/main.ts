0c9f5c1173f8c51c9d9adb76d4c05e1d5fbcb7b5
lucky <101040016+luckylca@users.noreply.github.com>
2026-08-05 19:41:30 +0800

    Load tutorial book styles

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './inventory.css';
 import './progression.css';
 import './survival.css';
 import './minecraft-ui.css';
+import './tutorial-book.css';
 import { bootstrap } from './app/bootstrap';
 
 void bootstrap();
