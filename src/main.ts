2c36f990841fffe32b89ef579d686d5cfb63dadb
lucky <101040016+luckylca@users.noreply.github.com>
2026-08-05 22:39:14 +0800

    Load biome and environment presentation styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css';
 import './inventory.css';
 import './progression.css';
 import './survival.css';
+import './environment.css';
 import './minecraft-ui.css';
 import './tutorial-book.css';
 import { bootstrap } from './app/bootstrap';
