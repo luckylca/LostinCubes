2c4d95fef74a564d45351eedd0119ab47e410d9e
lucky <101040016+luckylca@users.noreply.github.com>
2026-08-06 23:24:35 +0800

    Load build badge styles

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import './survival.css';
 import './environment.css';
 import './minecraft-ui.css';
 import './tutorial-book.css';
+import './build-version.css';
 import { bootstrap } from './app/bootstrap';
 import { initializeBuildBadge } from './buildInfo';
 
